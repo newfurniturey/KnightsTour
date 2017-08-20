4df0d03cc49f458d1a83f0eeea4be3843e50b2c1
newfurniturey <newfurniturey@gmail.com>
2017-08-20 02:02:34 -0400

    Adding a few helper methods

--- KnightsTour.cs
+++ KnightsTour.cs
@@ -9,7 +9,8 @@ namespace com.newfurniturey.KnightsTour {
 
 		private uint boardWidth = 8;
 		private uint boardHeight = 8;
-		private int[,] board;
+		private uint[,] board;
+		private uint currentMove = 1;
 
 		public uint BoardWidth {
 			get { return boardWidth; }
@@ -19,10 +20,24 @@ namespace com.newfurniturey.KnightsTour {
 			get { return boardHeight; }
 		}
 
-		public KnightsTour(uint width = 8, uint height = 8) {
-			boardWidth = width;
-			boardHeight = height;
+		public KnightsTour(uint rows = 8, uint columns = 8) {
+			boardWidth = columns;
+			boardHeight = rows;
 			initializeBoard();
+		}
+
+		public bool IsBoardComplete() => (currentMove == (BoardWidth * BoardHeight) + 1);
+
+		public bool IsBoardReallyComplete() {
+			for (uint row = 0; row < BoardHeight; row++) {
+				for (uint col = 0; col < BoardWidth; col++) {
+					if (!isTaken(row, col)) {
+						return false;
+					}
+				}
+			}
+
+			return true;
 		}
 
 		public void PrintBoard() {
@@ -38,7 +53,19 @@ namespace com.newfurniturey.KnightsTour {
 		}
 
 		private void initializeBoard() {
-			board = new int[BoardWidth,BoardHeight];
+			board = new uint[BoardWidth,BoardHeight];
+		}
+
+		private bool isTaken(uint row, uint column) {
+			return (board[row, column] != 0);
+		}
+
+		private bool isValidPosition(uint row, uint column) {
+			return (row < BoardHeight) && (column < BoardWidth);
+		}
+
+		private void move(uint row, uint column) {
+			board[row, column] = currentMove++;
 		}
 	}
 }
